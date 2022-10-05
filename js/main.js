702a892a737555586cde900c24c9f8407121bb71
tamarithethird <tamari.bablidze@gmail.com>
2022-10-06 02:39:43 +0400

    change

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $('nav').load('/navbar.html');
+  // $('footer').load('/footer.html');
+
+  let pathname = window.location.pathname;
+
+  if (pathname == '/index.html' && window.history.length == 1) {
+    $('.nav').hide();
+    $('.menu').click(function (e) {
+      e.preventDefault();
+      $('.nav').show();
+    });
+  } else {
+    $('.menu').click(function (e) {
+      e.preventDefault();
+      $('.nav').toggle();
+    });
+  }
+
+  let doc_height = $(document).innerHeight();
+
+  $(window).scroll(function () {
+    if (window.scrollY >= doc_height / 2) {
+      $('.nav').hide();
+    }
+  });
+
+  document.querySelectorAll('.nav-link').forEach((link) => {
+    if (link.href.includes(`${pathname}`)) {
+      link.classList.add('active');
+      console.log(link);
+    }
+  });
+});
--- js/navbar.js
+++ js/navbar.js
@@ -1,24 +1,39 @@
 $(document).ready(function () {
-  let pathname = window.location.pathname;
+  $('.nav').hide();
 
-  if (pathname == '/index.html' && window.history.length == 1) {
-    $('.nav').hide();
-    $('.menu').click(function () {
-      //       e.preventDefault();
-      $('.nav').show();
-    });
-  } else {
-    $('.menu').click(function () {
-      //       e.preventDefault();
-      $('.nav').toggle();
-    });
-  }
+  $('.menu').click(function (e) {
+    e.preventDefault();
+    $('.nav').toggle();
+  });
+
+  // let pathname = window.location.pathname;
+
+  // if (pathname == '/index.html' && window.history.length == 1) {
+  //   $('.nav').hide();
+  //   $('.menu').click(function (e) {
+  //     e.preventDefault();
+  //     $('.nav').show();
+  //   });
+  // } else {
+  //   $('.menu').click(function (e) {
+  //     e.preventDefault();
+  //     $('.nav').toggle();
+  //   });
+  // }
 
   let doc_height = $(document).innerHeight();
 
   $(window).scroll(function () {
     if (window.scrollY >= doc_height / 2) {
       $('.nav').hide();
+    }
+  });
+
+  document.querySelectorAll('.nav-link').forEach((link) => {
+    if (link.href.includes(`${pathname}`)) {
+      link.classList.add('active');
+
+      console.log(link);
     }
   });
 });
